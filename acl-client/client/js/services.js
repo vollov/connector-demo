8fcfbcb0c0c9e5a91b2731e8bd8d64566bbd7719
Dustin Zhang <vollov.ca@gmail.com>
2013-10-28 16:23:58 -0400

    test first http request

--- acl-client/client/js/services.js
+++ acl-client/client/js/services.js
@@ -26,39 +26,41 @@ demoApp.factory('SessionService', function(){
 
 demoApp.factory('AuthenticationService', function($http, $location,
 		SessionService, FlashService) {
-//	var cacheSession = function() {
-//		SessionService.set('tid', true);
-//	};
-//
-//	var uncacheSession = function() {
-//		SessionService.unset('tid');
-//	};
-//
-//	var loginError = function(response) {
-//		FlashService.show(response.flash);
-//	};
+	var cacheSession = function(value) {
+		console.log('calling cacheSession with tid=' + value);
+		SessionService.set('tid', value);
+	};
+
+	var uncacheSession = function() {
+		SessionService.unset('tid');
+	};
+
+	var loginError = function(response) {
+		FlashService.show(response.flash);
+	};
 
 	// these routes map to stubbed API endpoints in config/server.js
 	return {
 		login : function(credentials) {
+			console.log('calling AuthenticationService.login! ');
 			// var login = $http.post('/api/login', credentials);
 			var login = $http({
-				url : httpRoot + '/api/login',
+				url : httpRoot + '/public/login',
 				method : 'POST',
 				data : credentials
 			});
-			login.success(cacheSession);
+			login.success(cacheSession('xxx'));
 			login.success(FlashService.clear);
 			login.error(loginError);
 			return login;
 		},
 		logout : function() {
-			var logout = $http.get('/api/logout');
+			var logout = $http.get('/public/logout');
 			logout.success(uncacheSession);
 			return logout;
 		},
 		isLoggedIn : function() {
-			return SessionService.get('authenticated');
+			return !(SessionService.get('tid') == null);
 		}
 	};
 });
--- acl-client/test/unit/servicesSpec.js
+++ acl-client/test/unit/servicesSpec.js
@@ -24,34 +24,50 @@ describe('Test all services in the acl-client', function() {
 	
 	describe('Test AuthenticationService', function() {
 		
-		var authenticationService;
+		var authenticationService, sessionService, $httpBackend;
+		var credentials = {
+				username: 'mary@demo.org',
+				password: 'passwd'
+		}
 		
 		beforeEach(function(){
 			module('appModule');
-			inject(function(AuthenticationService) {
+			inject(function(AuthenticationService, SessionService) {
 				authenticationService = AuthenticationService;
+				sessionService = SessionService;
+			}, function(_$httpBackend_){
+				$httpBackend = _$httpBackend_;
 			});
 		});
 		
-		it('should be able to check if logged in', function() {
-//			expect(sessionService.get('tokenid')).toBeNull();
-//			sessionService.set('tid', 'f2cb3e8d653f46008272113c6c72422843902ef8');
-//			//console.log('get tokenid = ' + sessionService.get('tokenid'));
-//			expect(sessionService.get('tid')).toBe('f2cb3e8d653f46008272113c6c72422843902ef8');
-//			sessionService.unset('tid');
-//			expect(sessionService.get('tid')).toBeNull();
+		it('logged in should be false if cookie tid is null', function() {
+			expect(authenticationService.isLoggedIn()).toBeFalsy();
 		});
 		
-		it('should be able to login success with good credentials', function() {
-			
+		it('logged in should be true if cookie tid is not null', function() {
+			sessionService.set('tid', 'f2cb3e8d653f46008272113c6c72422843902ef8');
+			expect(authenticationService.isLoggedIn()).toBeTruthy();
+			sessionService.unset('tid');
+			expect(authenticationService.isLoggedIn()).toBeFalsy();
 		});
 		
-		it('should be able to login fail with bad credentials', function() {
-			
+		it('should be able to login success when http return 200', function() {
+			//$httpBackend.flush();
+			$httpBackend.expectPOST('http://localhost:3000/public/login', credentials)
+			.respond(200, 
+					{ tokenid : 'f2cb3e8d653f46008272113c6c72422843902ef8'});
+			authenticationService.login(credentials);
+			expect(authenticationService.isLoggedIn()).toBeTruthy();
+			sessionService.unset('tid');
+			expect(authenticationService.isLoggedIn()).toBeFalsy();
 		});
-		
-		it('should be able to logout in any condition', function() {
-			
-		});
+//		
+//		it('should be able to login fail with bad credentials', function() {
+//			
+//		});
+//		
+//		it('should be able to logout in any condition', function() {
+//			
+//		});
 	});
 });
